ffca848215bfc6c234bdf80aac5e1f04fba13298
tirthmodi <skpimca.tirthmodi@gmail.com>
2026-01-07 15:40:25 +0530

    Initial commit: simple Java project

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps Internship");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/main/java/AppTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
