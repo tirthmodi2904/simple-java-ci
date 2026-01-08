d38f8ea16ed0c37daec232fdf3499290bfc1b9a6
tirthmodi <skpimca.tirthmodi@gmail.com>
2026-01-08 10:59:22 +0530

    uncomment for fail

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,21 @@
 public class App {
+
     public static void main(String[] args) {
         System.out.println("Hello DevOps Internship");
+
+        int x1 = 10 / 0;
+        int x2 = 20 / 0;
+        int x3 = 30 / 0;
+        int x4 = 40 / 0;
+        int x5 = 50 / 0;
+        int x6 = 60 / 0;
+        int x7 = 70 / 0;
+        int x8 = 80 / 0;
+        int x9 = 90 / 0;
+        int x10 = 100 / 0;
+        int x11 = 110 / 0;
+        
+
     }
 
     public static int add(int a, int b) {
